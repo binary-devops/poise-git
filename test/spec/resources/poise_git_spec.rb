b559664a834e6e9b210f93f7f6798795ad38342f
Noah Kantrowitz <noah@coderanger.net>
2017-03-27 02:29:03 -0700

    Actual tests for poise_git resource.

--- test/spec/resources/poise_git_spec.rb
+++ test/spec/resources/poise_git_spec.rb
@@ -17,14 +17,10 @@
 require 'spec_helper'
 
 describe PoiseGit::Resources::PoiseGit do
-  recipe do
-    poise_git_client 'git'
-    poise_git '/test' do
-      user 'root'
-      repository 'https://example.com/test.git'
-      revision 'd44ec06d0b2a87732e91c005ed2048c824fd63ed'
-      deploy_key 'secretkey'
-    end
+  step_into(:poise_git)
+  before do
+    override_attributes['poise-git'] ||= {}
+    override_attributes['poise-git']['provider'] = 'dummy'
   end
 
   before do
@@ -32,5 +28,78 @@ describe PoiseGit::Resources::PoiseGit do
     allow_any_instance_of(described_class::Provider).to receive(:action_sync).and_return(nil)
   end
 
-  it { run_chef }
+  def cache_path(path)
+    "#{Chef::Config[:file_cache_path]}/#{path}"
+  end
+
+  context 'with defaults' do
+    recipe do
+      poise_git_client 'git'
+      poise_git '/test' do
+        repository 'https://example.com/test.git'
+        revision 'd44ec06d0b2a87732e91c005ed2048c824fd63ed'
+      end
+    end
+
+    it { is_expected.to_not render_file(cache_path('poise_git_deploy_2089348824')) }
+    it { is_expected.to_not render_file(cache_path('poise_git_wrapper_2089348824')) }
+  end # /context with defaults
+
+  context 'with in-line deploy key' do
+    recipe do
+      poise_git_client 'git'
+      poise_git '/test' do
+        repository 'https://example.com/test.git'
+        revision 'd44ec06d0b2a87732e91c005ed2048c824fd63ed'
+        deploy_key 'secretkey'
+      end
+    end
+
+    it { is_expected.to render_file(cache_path('poise_git_deploy_2089348824')).with_content('secretkey') }
+    it { is_expected.to render_file(cache_path('poise_git_wrapper_2089348824')).with_content(%Q{#!/bin/sh\n/usr/bin/env ssh -o "StrictHostKeyChecking=no" -i "#{cache_path('poise_git_deploy_2089348824')}" $@\n}) }
+    it { expect(chef_run.poise_git('/test').to_text).to include 'deploy_key "suppressed sensitive value"' }
+  end # /context with in-line deploy key
+
+  context 'with path deploy key' do
+    recipe do
+      poise_git_client 'git'
+      poise_git '/test' do
+        repository 'https://example.com/test.git'
+        revision 'd44ec06d0b2a87732e91c005ed2048c824fd63ed'
+        deploy_key '/mykey'
+      end
+    end
+
+    it { is_expected.to_not render_file(cache_path('poise_git_deploy_2089348824')) }
+    it { is_expected.to render_file(cache_path('poise_git_wrapper_2089348824')).with_content(%Q{#!/bin/sh\n/usr/bin/env ssh -o "StrictHostKeyChecking=no" -i "/mykey" $@\n}) }
+    it { expect(chef_run.poise_git('/test').to_text).to include 'deploy_key "/mykey"' }
+  end # /context with path deploy key
+
+  context 'with no parent' do
+    recipe do
+      poise_git '/test' do
+        repository 'https://example.com/test.git'
+        revision 'd44ec06d0b2a87732e91c005ed2048c824fd63ed'
+      end
+    end
+
+    it { is_expected.to install_poise_git_client('git') }
+  end # /context with no parent
+
+  context 'without an explicit revision' do
+    # This test confirms the execution plumbing is working because
+    # load_current_resource will try to ls-remote.
+    recipe do
+      poise_git_client 'git'
+      poise_git '/test' do
+        repository 'https://example.com/test.git'
+      end
+    end
+
+    it do
+      fake_cmd = double(error!: nil, stdout: "d44ec06d0b2a87732e91c005ed2048c824fd63ed\tHEAD\n")
+      expect_any_instance_of(described_class::Provider).to receive(:poise_shell_out).with(%w{/git ls-remote https://example.com/test.git HEAD}, log_tag: 'poise_git[/test]', timeout: 900, environment: {}).and_return(fake_cmd)
+      run_chef
+    end
+  end # /context without an explicit revision
 end
