384729f6ba5d44d96eb4132d03cafb94e5926366
Noah Kantrowitz <noah@coderanger.net>
2017-03-27 02:41:34 -0700

    Update copyright headers from old copy-pasta.

--- lib/poise_git/git_client_providers/system.rb
+++ lib/poise_git/git_client_providers/system.rb
@@ -1,6 +1,5 @@
 #
-# Copyright 2008-2017, Chef Software, Inc.
-# Copyright 2015-2017, Noah Kantrowitz
+# Copyright 2017, Noah Kantrowitz
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
